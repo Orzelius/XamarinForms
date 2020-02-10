55fd8025b78a7889df397b801c986b3f547590f0
Albert Kostusev <albertkostusev@gmail.com>
2020-02-10 19:09:21 +0200

    update

---
+++ MVVM/MVVM/MVVM/View Models/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MVVM.ViewModels {
+    public class MainViewModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        private string _input;
+        private double _tip;
+
+        public string Tip {
+            get {
+                var num = _tip.ToString().Split('.')[0];
+                return num;
+            }
+        }
+        public string Input {
+            get { return _input; }
+            set {
+                _input = value;
+                _tip = Convert.ToDouble(value) * 0.8f;
+                OnPropertyChanged(nameof(Input));
+                OnPropertyChanged(nameof(Tip));
+            }
+        }
+    }
+}
